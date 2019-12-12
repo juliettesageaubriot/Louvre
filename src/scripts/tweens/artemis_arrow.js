fd92aef58a3c09810c5fc9456a46e2b760643739
An Truong <antr.2201@gmail.com>
2019-12-12 20:47:08 +0100

    adjust time

--- src/scripts/tweens/artemis_arrow.js
+++ src/scripts/tweens/artemis_arrow.js
@@ -4,7 +4,6 @@ import tweenArtemis from './artemis';
 import tweenArrow from './arrow';
 
 const DELAY_START_ARROW = 0.8;
-const DELAY_END_ARTEMIS = 0.1;
 const DURATION_ARTEMIS = 2.4;
 
 const tween = (appScroller, autoScroll = true) => {
@@ -25,7 +24,7 @@ const tween = (appScroller, autoScroll = true) => {
 			null,
 			'+=0.1'
 		)
-		.call(artemis.pause, null, DURATION_ARTEMIS - DELAY_END_ARTEMIS)
+		.call(artemis.pause, null, DURATION_ARTEMIS)
 		.call(
 			() => {
 				if (doFireArrow) {
